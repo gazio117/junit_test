5cd4258fc4d109941cb4467cb458b04e1f65f77b
hoony <gazio117@naver.com>
2022-08-17 00:06:57 +0900

    북서비스 책한건보기 완료

--- src/main/java/com/miroku/junit/service/BookService.java
+++ src/main/java/com/miroku/junit/service/BookService.java
@@ -1,6 +1,7 @@
 package com.miroku.junit.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -35,6 +36,14 @@ public class BookService {
     }
 
     // 3. 책한건보기
+    public BookRespDto 책한건보기(Long id) {
+        Optional<Book> bookPS = bookRepository.findById(id);
+        if (bookPS.isPresent()) {
+            return new BookRespDto().toDto(bookPS.get());
+        } else {
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+    }
 
     // 4. 책삭제
 
