4de2cb21f09f5fae19af62e215fafb2008f5e222
hoony <gazio117@naver.com>
2022-08-17 00:01:26 +0900

    북서비스 책목록보기 완료

--- src/main/java/com/miroku/junit/service/BookService.java
+++ src/main/java/com/miroku/junit/service/BookService.java
@@ -1,5 +1,9 @@
 package com.miroku.junit.service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +28,11 @@ public class BookService {
     }
 
     // 2. 책목록보기
+    public List<BookRespDto> 책목록보기() {
+        return bookRepository.findAll().stream()
+                                                    .map(new BookRespDto()::toDto)
+                                                    .collect(Collectors.toList());
+    }
 
     // 3. 책한건보기
 
