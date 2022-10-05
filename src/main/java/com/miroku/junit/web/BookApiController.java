30bd6ea69dc0ef64ea0516a175d87178fc9c5094
hoony <gazio117@naver.com>
2022-10-06 00:15:29 +0900

    책 한건보기 컨트롤러 완료

--- src/main/java/com/miroku/junit/web/BookApiController.java
+++ src/main/java/com/miroku/junit/web/BookApiController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,8 +54,10 @@ public class BookApiController {
     }
     
     // 3. 책한건보기
-    public ResponseEntity<?> getBookOne() {
-        return null;
+    @GetMapping("api/v1/book/{id}")
+    public ResponseEntity<?> getBookOne(@PathVariable Long id) {
+        BookRespDto bookRespDto = bookService.책한건보기(id);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 한건보기 성공").body(bookRespDto).build(), HttpStatus.OK);
     }
     
     // 4. 책삭제
