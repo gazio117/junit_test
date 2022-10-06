8617336734020e2391725876e66974df723ec37c
hoony <gazio117@naver.com>
2022-10-07 00:44:59 +0900

    책 조회하기 통합테스트(컨트롤러) 완료

--- src/test/java/com/miroku/junit/web/BookApiControllerTest.java
+++ src/test/java/com/miroku/junit/web/BookApiControllerTest.java
@@ -1,7 +1,7 @@
 package com.miroku.junit.web;
 
-import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,16 +12,24 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.miroku.junit.domain.Book;
+import com.miroku.junit.domain.BookRepository;
 import com.miroku.junit.web.dto.request.BookSaveReqDto;
+
+import static org.assertj.core.api.Assertions.*;
+
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BookApiControllerTest {
     
+    @Autowired
+    private BookRepository bookRepository;
 
     @Autowired
     private TestRestTemplate rt;
@@ -34,6 +42,18 @@ public class BookApiControllerTest {
         mapper = new ObjectMapper();
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void 데이터준비() {
+        String title = "junit5";
+        String author = "테스트";
+
+        Book book  = Book.builder()
+                                .title(title)
+                                .author(author)
+                                .build();
+        bookRepository.save(book);
     }
     
     @Test
@@ -56,6 +76,27 @@ public class BookApiControllerTest {
 
         assertThat(title).isEqualTo("타이틀입니다.");
         assertThat(author).isEqualTo("작가입니다.");
+
+    }
+
+    @Sql("classpath:db/tableInit.sql")  // id로 검증시 autoIncrement 로 인한 오류를 방지 하기 위해 테이블 재생성..
+    @Test
+    public void getBookList_test() {
+        //given
+
+        //when
+        HttpEntity<String> request = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.GET, request, String.class);
+
+        System.out.println(response.getBody());
+
+        //then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        String title = dc.read("$.body.items[0].title");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("junit5");
 
     }
 }
